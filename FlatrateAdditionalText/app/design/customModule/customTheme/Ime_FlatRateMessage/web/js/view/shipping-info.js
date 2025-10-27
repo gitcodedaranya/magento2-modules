de50349d55149c40ad1cfb1de7409740509cefce
WebdevAD <aranya.webdeveloper@gmail.com>
2025-10-27 23:37:50 +0530

    Flatrate Additional message
    magento 2 display custom message under flatrate shipping method.
    magento 2 display message to a specific shipping method.
    Flatrate additional text message
    shipping method message

---
+++ FlatrateAdditionalText/app/design/customModule/customTheme/Ime_FlatRateMessage/web/js/view/shipping-info.js
@@ -0,0 +1,35 @@
+define([
+   'uiComponent',
+   'ko',
+   'Magento_Checkout/js/model/quote'
+], function (Component, ko, quote) {
+   'use strict';
+   return Component.extend({
+       defaults: {
+           template: 'Ime_FlatRateMessage/shipping-info'
+       },
+       initObservable: function () {
+           var self = this._super();
+           /* this.showFreeShippingInfo = ko.computed(function() {
+               var method = quote.shippingMethod();
+               //Here you can add your logic ....
+               if(method && method['carrier_code'] !== undefined) {
+                   if(method['carrier_code'] == 'freeshipping') {
+                       return true;
+                   }
+               }
+               return false;
+           }, this); */
+           this.showTableRateShippingInfo = ko.computed(function() {
+               var method = quote.shippingMethod();
+               if(method && method['carrier_code'] !== undefined) {
+                   if(method['carrier_code'] == 'flatrate') {
+                       return true;
+                   }
+               }
+               return false;
+           }, this);
+           return this;
+       }
+   });
+});
